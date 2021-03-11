27d3ec99025236a01aed6309796713fee5fca0a1
zhaoran <zhao-ran.ding@sap.com>
2021-03-11 10:22:33 +0100

    # modify import

--- src/main/mrsequential.go
+++ src/main/mrsequential.go
@@ -7,7 +7,7 @@ package main
 //
 
 import "fmt"
-import "../mr"
+import "awesomeProject/MIT-6.824/src/mr"
 import "plugin"
 import "os"
 import "log"
--- src/mrapps/wc.go
+++ src/mrapps/wc.go
@@ -6,7 +6,7 @@ package main
 // go build -buildmode=plugin wc.go
 //
 
-import "../mr"
+import "awesomeProject/MIT-6.824/src/mr"
 import "unicode"
 import "strings"
 import "strconv"
